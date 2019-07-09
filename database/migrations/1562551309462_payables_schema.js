e507f2ca763d8a4fd187095cd7179071e9b8733e
Carlos Henrique Reis <carlos_henreis@unifei.edu.br>
2019-07-09 00:41:51 -0300

    Adcionando correlações entre as tabelas

--- database/migrations/1562551309462_payables_schema.js
+++ database/migrations/1562551309462_payables_schema.js
@@ -7,6 +7,18 @@ class PayablesSchema extends Schema {
   up () {
     this.create('payables', (table) => {
       table.increments()
+      table.integer('user_id')
+           .unsigned()
+           .references('id')
+           .inTable('users')
+           .onUpdate('CASCADE')
+           .onDelete('CASCADE')
+      table.integer('transaction_id')
+           .unsigned()
+           .references('id')
+           .inTable('transactions')
+           .onUpdate('CASCADE')
+           .onDelete('CASCADE')
       table.enu('status', ['paid', 'waiting_funds']).notNullable()
       table.date('payment_date').notNullable()
       table.timestamps()
